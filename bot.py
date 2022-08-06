da79837e87b7189b583940c502fbc9fe1b8703dc
Efreitor2001 <pokimoha.123@gmail.com>
2022-08-06 10:09:14 +0300

    Done 9

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
+from aiogram.utils import executor
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
 
-bot = Bot('5439181005:AAGjlNaqXx5toLp7aDiSfBSl7OIbkI13KY0')
+bot = Bot('5490678266:AAGHhzuse7TRRojzitQir_qZVp-t3u4VWok')
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=['new_chat_members'])
+async def del_new(message):
+    await bot.delete_message(message.chat.id, message.message_id)
 
 
 @dp.message_handler(content_types=['text'])
 async def delete_messages(message: types.Message):
     check = 0
     is_admin = await bot.get_chat_administrators(message.chat.id)
-    wl = ['https://t.me/c/', 'https://github.com']
+    wl = ['https://t.me/c/', 'github.com']  # withe list
     for i in range(len(is_admin)):
         if int(message.from_user.id) == int(is_admin[i]['user']['id']):
             check = 1
