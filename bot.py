d315d0ceadb5db91cca34d91a78ea5b188449910
Efreitor2001 <pokimoha.123@gmail.com>
2022-08-06 22:01:27 +0300

    Done 14

--- bot.py
+++ bot.py
@@ -11,21 +11,31 @@ async def del_new(message):
     await bot.delete_message(message.chat.id, message.message_id)
 
 
-@dp.message_handler(content_types=types.ContentTypes.ANY)
+@dp.message_handler(content_types=['any'])
 async def delete_messages(message: types.Message):
     check = 0
     is_admin = await bot.get_chat_administrators(message.chat.id)
-    wl = ['https://t.me/c/', 'github.com']  # withe list
+    wl = ['https://t.me/c/', 'github.com']
     for i in range(len(is_admin)):
         if int(message.from_user.id) == int(is_admin[i]['user']['id']):
             check = 1
-    for i in wl:
-        if i in message.text:
-            check = 1
+    if message.content_type == 'photo':
+        for i in wl:
+            if i in message.caption:
+                check = 1
+    else:
+        for i in wl:
+            if i in message.text:
+                check = 1
     if check == 0:
-        for entity in message.entities:
-            if entity.type in ["url", "text_link", "hlink"]:
-                await message.delete()
+        if message.content_type == 'photo':
+            for caption_entity in message.caption_entities:
+                if caption_entity.type in ["url", "text_link", "hlink"]:
+                    await message.delete()
+        else:
+            for entity in message.entities:
+                if entity.type in ["url", "text_link", "hlink"]:
+                    await message.delete()
 
 
 print('run')
