749a9f362f530d22ce7b94db888a97e0dd74f7b3
Efreitor2001 <pokimoha.123@gmail.com>
2022-08-14 21:40:44 +0300

    remove tokens

--- create_bot.py
+++ create_bot.py
@@ -1,5 +1,5 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 
-bot = Bot('5490678266:AAGHhzuse7TRRojzitQir_qZVp-t3u4VWok')
+bot = Bot('5440800900:AAGgpVQ17urG0bk5zirYU1gH5OSRwAS000U')
 dp = Dispatcher(bot)
