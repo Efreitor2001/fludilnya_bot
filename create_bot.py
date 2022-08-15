b7a19cd587527ca12848ac5910bd99e03b7ce6df
Efreitor2001 <pokimoha.123@gmail.com>
2022-08-15 18:53:00 +0300

    23

--- create_bot.py
+++ create_bot.py
@@ -1,5 +1,5 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 
-bot = Bot('5440800900:AAGgpVQ17urG0bk5zirYU1gH5OSRwAS000U')
+bot = Bot('5440800900:AAEa36GV_PWj7fORMK3kWt4eB3OmWYmFUYY')
 dp = Dispatcher(bot)
